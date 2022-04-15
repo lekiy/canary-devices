d26eefb2dd5199c11f2139363223b93b1692ed1f
Kevin Annen <lekiyminin@gmail.com>
2022-04-14 20:47:09 -0500

    Added Temperature Chart

--- src/UI/DeviceViewer.js
+++ src/UI/DeviceViewer.js
@@ -1,18 +1,27 @@
 import { Box, Typography } from "@mui/material";
 import React from "react";
 import useDeviceQuery from "../Util/Hooks/useDeviceQuery";
+import TempChart from "./TempChart";
 
 function DeviceViewer({id}){
 
     const deviceReadings = useDeviceQuery(`/devices/${id}/readings`);
-    const deviceData = useDeviceQuery(`/devices/${id}`);
+    const deviceData = useDeviceQuery(`/devices/${id}`);   
     
+    const temperatureData = deviceReadings.filter(reading => reading.type === "temperature").map(reading => {
+        return {
+            name: reading.created,
+            temp: reading.value
+        };
+    });
+
     if(id === undefined) return;
 
     return (
     <Box m={"20px"} ml={"270px"} p={"10px"} backgroundColor="white">
         <Typography variant="h2">{deviceData.name}</Typography>
-        {deviceReadings.map(reading => `value: ${reading.value} `)}
+        <TempChart temperatureData={temperatureData} />
+        {/* {deviceReadings.map(temps => `temps: ${temps} `)} */}
     </Box>);
 }
 
