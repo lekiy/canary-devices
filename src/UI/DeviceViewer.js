873e3938a3f034a69662dc85e1f307391b6a396f
Kevin Annen <lekiyminin@gmail.com>
2022-04-19 09:20:18 -0500

    Cleaned up unused import

--- src/UI/DeviceViewer.js
+++ src/UI/DeviceViewer.js
@@ -1,4 +1,4 @@
-import { Box, Typography, Grid, Button } from "@mui/material";
+import { Box, Typography, Grid } from "@mui/material";
 import React from "react";
 import useAPIQuery from "../Util/Hooks/useAPIQuery";
 import DataChart from "./DataChart";
